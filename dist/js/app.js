a9f3847cbd33be837761aa2a6fa8510233b3ebee
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-16 10:55:32 +0100

    - Create card html structure in Excellence section - Roughly set data structure  in app.js to dynamically insert elements.

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,12 @@
 var app = new Vue({
   el: "#root",
   data: {
-    navLinks: ["home", "about", "services", "process", "team", "blog"]
+    navLinks: ["home", "about", "services", "process", "team", "blog"],
+    servicesCards: [{
+      service: 'Audit & Assurance',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      icon: 'fas fa-project-diagram'
+    }]
   },
   methods: {}
 });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const app = new Vue({
     el: "#root",
 
     data: {
+
         navLinks: [
             "home",
             "about",
@@ -9,6 +10,14 @@ const app = new Vue({
             "process",
             "team",
             "blog"
+        ],
+
+        servicesCards:[
+            {
+                service: 'Audit & Assurance',                
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+                icon:'fas fa-project-diagram'
+            },
         ]
     },
 
