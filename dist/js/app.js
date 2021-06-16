ba8d316db2ff162d832675c0da0882f10d00e9df
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-16 12:05:47 +0100

    - Dynamically create  all cards in Excellence sect - Style cards in excellence.scss partial.

--- dist/js/app.js
+++ dist/js/app.js
@@ -15,6 +15,26 @@ var app = new Vue({
       service: 'Audit & Assurance',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
       icon: 'fas fa-project-diagram'
+    }, {
+      service: 'Financial Advisory',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      icon: 'fas fa-briefcase'
+    }, {
+      service: 'Analytics and M&A',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      icon: 'fas fa-chart-bar'
+    }, {
+      service: 'Middle Marketing',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      icon: 'fas fa-paper-plane'
+    }, {
+      service: 'Legal Consulting',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      icon: 'fas fa-globe'
+    }, {
+      service: 'Regulatory Risk',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      icon: 'fas fa-inbox'
     }]
   },
   methods: {}
--- src/app.js
+++ src/app.js
@@ -18,6 +18,31 @@ const app = new Vue({
                 description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
                 icon:'fas fa-project-diagram'
             },
+            {
+                service: 'Financial Advisory',                
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+                icon:'fas fa-briefcase'
+            },
+            {
+                service: 'Analytics and M&A',                
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+                icon:'fas fa-chart-bar'
+            },
+            {
+                service: 'Middle Marketing',                
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+                icon:'fas fa-paper-plane'
+            },
+            {
+                service: 'Legal Consulting',                
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+                icon:'fas fa-globe'
+            },
+            {
+                service: 'Regulatory Risk',                
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+                icon:'fas fa-inbox'
+            },
         ]
     },
 
